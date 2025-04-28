66a30bc8bd61a78e9a81b2d405f2790cb4f4b5cc
Kartikey Mishra <kartikeymishracsjm@gmail.com>
2025-04-29 01:16:04 +0530

    fixed error

--- src/app/api/projects/[projectId]/tasks/[taskId]/route.ts
+++ src/app/api/projects/[projectId]/tasks/[taskId]/route.ts
@@ -4,27 +4,33 @@ import Task from '@/models/Task';
 import { auth } from '@/auth';
 import type { NextRequest } from 'next/server';
 
-export async function DELETE(request: NextRequest, context: { params: { projectId: string; taskId: string } }) {
-  const session = await auth();
-  if (!session) return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { projectId: string; taskId: string } }
+) {
+    const session = await auth();
+    if (!session) return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
 
-  const taskId = context.params.taskId;
-  await Connect();
-  await Task.findByIdAndDelete(taskId);
+    const { taskId } = params;
+    await Connect();
+    await Task.findByIdAndDelete(taskId);
 
-  return NextResponse.json({ message: 'Task deleted successfully' });
+    return NextResponse.json({ message: 'Task deleted successfully' });
 }
 
-export async function PUT(request: NextRequest, context: { params: { projectId: string; taskId: string } }) {
-  const session = await auth();
-  if (!session) return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
+export async function PUT(
+  request: NextRequest,
+  { params }: { params: { projectId: string; taskId: string } }
+) {
+    const session = await auth();
+    if (!session) return NextResponse.json({ message: 'Unauthorized' }, { status: 401 });
 
-  const taskId = context.params.taskId;
-  const body = await request.json();
-  const { title, description, status } = body;
+    const { taskId } = params;
+    const body = await request.json();
+    const { title, description, status } = body;
 
-  await Connect();
-  const task = await Task.findByIdAndUpdate(taskId, { title, description, status }, { new: true });
+    await Connect();
+    const task = await Task.findByIdAndUpdate(taskId, { title, description, status }, { new: true });
 
-  return NextResponse.json({ task });
+    return NextResponse.json({ task });
 }
